3e78d3a4e48ced6b72a0d1d434e53a330c6adc88
Björn Ekengren <bjorn@virtusize.com>
2018-12-19 16:26:28 +0100

    Feature: Updated dist files.

--- dist/hello.all.js
+++ dist/hello.all.js
@@ -5927,6 +5927,40 @@ if (typeof chrome === 'object' && typeof chrome.identity === 'object' && chrome.
 
 })(hello);
 
+// Line
+(function(hello) {
+	// For APIs, once a version is no longer usable, any calls made to it will be defaulted to the next oldest usable version.
+	// So we explicitly state it.
+	var version = 'v2.1';
+
+	hello.init({
+
+		line: {
+
+			name: 'Line',
+
+			// SEE https://developers.line.biz/en/docs/line-login/web/integrate-line-login/
+			oauth: {
+				version: 2,
+				auth: 'https://access.line.me/oauth2/' + version + '/authorize?response_type=code',
+				response_type: 'code',
+				grant: 'https://api.line.me/oauth2/' + version + '/token'
+			},
+
+			// Authorization scopes
+			scope: {
+				basic: 'profile',
+				email: 'openid email'
+			},
+
+			scope_delim: ' ',
+
+			base: 'https://api.line.me/'
+		}
+	});
+
+})(hello);
+
 // Register as anonymous AMD module
 if (typeof define === 'function' && define.amd) {
 	define(function() {
